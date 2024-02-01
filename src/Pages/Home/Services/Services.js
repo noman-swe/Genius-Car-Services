e2e47dc3b821976290b701d711c5629b85e55bf0
noman-swe <noman.swe130@gmail.com>
2024-02-01 19:25:17 +0600

    createed services data, and img Collect, Edit, Optimize And Host Images For Your Website

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Services = () => {
+    const services = [
+        { id: 1, name: 'Oil Change', price: 100, description: 'Changes/Drain the oil first then wash and after that new oil is given.', img: 'https://ibb.co/Nsg2CJh' },
+
+    ]
+    return (
+        <div>
+            <h3>Services</h3>
+        </div>
+    );
+};
+
+export default Services;
