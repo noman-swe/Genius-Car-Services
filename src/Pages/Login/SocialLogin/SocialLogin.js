5865cdcf657ab8fe1254a9e0d3b10554ef77ca42
noman-swe <noman.swe130@gmail.com>
2024-02-22 13:55:11 +0600

    3 Implement Google Login System

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -4,17 +4,36 @@ import googleLogo from '../../../images/logos/google-logo.png';
 import fbLogo from '../../../images/logos/fb.png';
 import githubLogo from '../../../images/logos/github.png';
 import './SocialLogin.css';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    let errorElement;
+    if (error) {
+        errorElement =
+            <div>
+                <p className='text-danger'>Error: {error.message}</p>
+            </div>
+    }
+
+    if (user) {
+        navigate('/home');
+    }
+
     return (
         <div>
+            {errorElement}
             <div className='d-flex justify-content-center align-items-center'>
                 <div className='bg-dark w-50 mx-2' style={{ height: '1px' }}></div>
                 <p className='' style={{ marginTop: '12px' }}>Or</p>
                 <div className='bg-dark w-50 mx-2' style={{ height: '1px' }}></div>
             </div>
             <div className="social-links">
-                <Button className='btn social-btn-google text-oranged w-100' type="submit">
+                <Button onClick={() => signInWithGoogle()} className='btn social-btn-google text-oranged w-100' type="submit">
                     Google Sign in<img height={25} src={googleLogo} alt="" />
                 </Button>
                 <Button className='mt-3 btn social-btn-fb text-oranged w-100' type="submit">
